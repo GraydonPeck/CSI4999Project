76424bff342494252fde91dd626d047340885a8d
wardo48446 <wardo48446@yahoo.com>
2017-09-08 09:51:19 -0400

    Added main page javascript
    Added java for main page with scroll feature on registration button press

---
+++ SportsFacility/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+	setBind();
+
+});
+
+function setBind(){
+	$('.cta').click(function(e){
+		e.preventDefault();
+		var sectionID = e.currentTarget.id+"Section";
+
+		$('body').animate({
+			scrollTop:$('#submitSection').offset().top
+		}, 3000)
+	});
+}
+
+$('#myModal').on('hidden.bs.modal', function () {
+ location.reload(true);
+})
+
+
+
