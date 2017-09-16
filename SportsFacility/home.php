48ac126bf24f793716911b9a2fe0c67282f72815
wardo48446 <wardo48446@yahoo.com>
2017-09-16 15:23:39 -0400

    Added home page base

---
+++ SportsFacility/home.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>HockeyPlex Home</title>
+  
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  
+  <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+  <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+  <script type = "text/javascript" src = "chk.js"></script>
+  <link rel="stylesheet" type="text/css" href="main.css">
+
+
+</head>
+
+<body>
+ <div>
+  <nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+        <span class="sr-only">Toggle navigation</span>
+      </button>
+    </div>
+      <ul class="nav navbar-nav" id="login-dp" >
+            <!-- Trigger Login Modal -->
+              <li class="active" data-toggle="modal" data-target="#Login"> <a href="#">Login</a></li>
+              <!-- End Trigger-->
+              <li><a href="home.php"><span class="glyphicon glyphicon-home"></span></a></li>
+       </ul>
+    </nav>
+
+  <div class="intro-section">
+    <div class="container info">
+      <h1><big>Hockety<strong>Plex</strong></big></h1>
+      <p><a class="btn btn-lg btn-success cta" href="#submitSection" role="button">Get started today</a></p>
+    </div>
+  </div>
+
+</html>
