6a74d79fb32c17273f68f6cb4596cd4ae2d56f05
wardo48446 <pbkowals@oakland.edu>
2017-09-19 15:31:24 +0000

    Added dbutils.php, chk.js and checklogin.php

---
+++ SportsFacility/checklogin.php
@@ -0,0 +1,27 @@
+<?php
+    include "dbutils.php";
+    session_start();
+ 
+	$username = $_POST['username'];
+	$passwd = $_POST['passwd'];
+
+    $retstatus = check_login ($username, $passwd, $msg);
+    echo $username . "<br>";
+    echo $passwd . "<br>";
+    echo $msg . "<br>";
+    echo "Return Status = " . $retstatus . "<br>";
+    if ($retstatus)
+    {
+	echo "Success " . "<br>";
+	$_SESSION['login'] = $username;
+	$goto = "Location: main.php";
+    } else {
+        echo "Failure " . "<br>";
+    	$ref = getenv("HTTP_REFERER");
+        $_SESSION['login'] = '';
+        //echo "<script language='javascript'> alert('Incorrect Password'); </script>";
+    	$goto = "Location: " . $ref;
+    }	
+	header($goto);
+
+?>
---
+++ SportsFacility/chk.js
@@ -0,0 +1,45 @@
+function valid()
+{
+    var reObj = document.getElementById("#formSection");
+    var UserName = reObj.User_name.value;
+    var Password = reObj.User_password.value;
+    var everythingOK = true;
+
+    if (!validateName(UserName))
+    {
+        alert("Error:Username was not entered.");
+        everythingOK = false;
+    }
+    
+    if (!validateName(Password))
+    {
+        alert("Error: Please enter a password.");
+        everythingOK = false;
+    }
+    
+    if (everythingOK)
+    {
+        alert("Thank you!.\nLog in using your new information!");
+        return true;
+    }
+    else
+        return false;
+}
+
+function validateName(UserName)
+{
+    var p = UserName.search(/^[-'\w\s]+$/);
+    if (p == 0)
+        return true;
+    else
+        return false;
+}
+
+function validateName(Password)
+{
+    var p = Password.search(/^[-'\w\s]+$/);
+    if (p == 0)
+        return true;
+    else
+        return false;
+}
