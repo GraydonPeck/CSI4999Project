ae6a6449b8f9f1b2a44a9ff97671ca5953b9b41d
wardo48446 <mpdegraeve@sbcglobal.net>
2017-09-22 20:25:53 +0000

    Changed how the navigation bar looked, changed what the table looks like, and added multiple functionalities, also added some inline style.

--- SportsFacility/customerpage.php
+++ SportsFacility/customerpage.php
@@ -1,10 +1,17 @@
 <!DOCTYPE html>
-
-<html lang="en">
 <?php
  include("dbutils.php");
  session_start();
+      if (count($_POST)) 
+    {
+
+	    echo "Found " . count($_POST) . " elements" . "<td>";
+        var_dump($_POST);
+        edit_customer ($_POST['user_number'], $_POST['customer_fname'], $_POST['customer_lname'], $_POST['customer_phone'], $_POST['customer_email'],  $_POST['customer_address'], $_POST['customer_city'],  $_POST['customer_state'],  $_POST['customer_country'],  $_POST['customer_zip'],  $_POST['customer_creditcard']);
+        header ("Location: customerpage.php");
+    }
 ?>
+<html lang="en">
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -20,82 +27,186 @@
   <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
   <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
   <script type = "text/javascript" src = "chk.js"></script>
-  <link rel="stylesheet" type="text/css" href="main.css">
+  <link rel="stylesheet" type="text/css" src="main.css">
 
+  <style>
+    body{
+     background-color:#cae4db; 
+    }
+     .navbar-default {
+    background-color: #dcae1d;
+    border-color: #030033;
+}
+
+ .dropdown-menu > li > a:hover,
+   .dropdown-menu > li > a:focus {
+    color: #262626;
+   text-decoration: none;
+  background-color: #7a9d96;  /*change color of links in drop down here*/
+   }
+   .dropdown-menu{
+     background-color:#dcae1d;
+   }
+   
+    .nav > li > a:hover,
+ .nav > li > a:focus {
+    text-decoration: none;
+    background-color: #00303f; /*Change rollover cell color here*/
+  }
+  
+  table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 50%;
+    margin:auto;
+}
+
+td, th {
+    border: 1px solid #dcae1d;
+    text-align: left;
+    padding:8px;
+}
+
+tr:nth-child(even) {
+    background-color: #dddddd;
+}
+
+.btn-default{
+  
+  margin-top: 2px;
+  
+}
+.btn-primary{
+  margin-top: 2px;
+}
+.navbar{
+  margin:0;
+  
+}
+.jumbotron{
+  background-color:#00303f;
+  color:white;
+}
+h1,h2,p{
+  text-align:center;
+}
+  </style>
 
 </head>
 
-<body>
+<body class="customer">
 
-<nav class="navbar navbar-inverse navbar-fixed-top">
+<nav class="navbar navbar-default " >
   <div class="container-fluid">
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-        <span class="sr-only">Toggle navigation</span>
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
       </button>
+      <a class="navbar-brand" href="main.php">SportPlex</a>
     </div>
-    <ul class="nav navbar-nav" id="login-dp" >
-            <!-- Trigger Login Modal -->
-              <li class="active" data-toggle="modal" data-target="#Login"> <a href="logout.php">Logout</a></li>
-              <!-- End Trigger-->
-              <li><a href="home.php"><span class="glyphicon glyphicon-home"></span></a></li>
-    </ul>
-    <ul class="nav navbar-nav navbar-right">
-          <!-- Username display -->
-          <?php 
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+      <li><a href="home.php"><span class="glyphicon glyphicon-home"></span></a></li>
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Schedule<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Events</a></li>
+            <li><a href="#">Times/Dates</a></li>
+            <li><a href="#">Your Schedule</a></li>
+          </ul>
+        </li>
+         <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Pro Shop<span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Viewed Items</a></li>
+          <li><a href="#">Items in Cart</a></li>
+          <li><a href="#">Searched Items</a></li>
+        </ul>
+        <li><a href="#">Video's</a></li>
+        <li><a href="#">About</a></li>
+         <li><a href="#"><span class="glyphicon glyphicon-mail"></span></a></li>
+        
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-trash"></span></a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-envelope"></span></a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-wrench"></span></a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span></a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-bell"></span></a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span></a></li>
+  
+        <?php
           echo "<li><a>" .$_SESSION['login']."</a></li>"
-          ;?>
+        ;?>
       </ul>
     </div>
-</nav>
-
-  <div class="EMP-section">
-    <div class="container info">
-      <h1><big>Hockey<strong>Plex</strong></big></h1>
-      <h2>This is customer page</h2>
-    </div>
   </div>
+
+</nav>
+<div class="jumbotron">
+  <h1><big>Hockey<strong>Plex</strong></big></h1>
+  <h2>This is customer page</h2>
+  <p>This is where the Customer enters extra information to their account.</p>
+</div>
   
+  
+      
    <div class = "container info">
-      <center>
+     <div class = "container-fluid">
+       <center>
+      <form id="#formSection" method="post" class="customeredit-form" data-animate="flipInX" action = "<?php echo $_SERVER['PHP_SELF']; ?>" onsubmit= "return valid()">
+      <input type ="hidden" name= "user_number" value="1">
       <table class = "">
-        
+         <tr>
+           <th>Information</th>
+           <th>Input</th>
+         </tr>
         <tr>
-          <td>First Name</td> <td><input type="text" name="fname"></td>
+          <td>First Name</td> <td><input type="text" name="customer_fname"></td>
         </tr>
         <tr>
-          <td>Last Name</td> <td><input type="text" name="lname"></td>
+          <td>Last Name</td> <td><input type="text" name="customer_lname"></td>
         </tr>
         <tr>
-          <td>Phone Number</td> <td><input type="text" name="phonenumber"></td>
+          <td>Phone Number</td> <td><input type="text" name="customer_phone"></td>
         </tr>
         <tr>
-          <td>Email</td> <td><input type="email" name="email"></td>
+          <td>Email</td> <td><input type="email" name="customer_email"></td>
         </tr>
         <tr>
-          <td>Address</td> <td><input type="text" name="address"></td>
+          <td>Address</td> <td><input type="text" name="customer_address"></td>
         </tr>
         <tr>
-          <td>City</td> <td><input type="text" name="city"></td>
+          <td>City</td> <td><input type="text" name="customer_city"></td>
         </tr>
         <tr>
-          <td>State</td> <td><input type="text" name"state"></td>
+          <td>State</td> <td><input type="text" name="customer_state"></td>
         </tr>
         <tr>
-          <td>Country</td> <td><input type="text" name="country"</td>
+          <td>Country</td> <td><input type="text" name="customer_country"</td>
         </tr>
         <tr>
-          <td>Zip</td> <td><input type="text" name="zip"></td>
+          <td>Zip</td> <td><input type="text" name="customer_zip"></td>
         </tr>
         <tr>
-          <td>Credit Card</td> <td><input type="text" name="creditcard"></td>
+          <td>Credit Card</td> <td><input type="text" name="customer_creditcard"></td>
         </tr>
-        
-     </center>  
       </table>
-
   
-
+     
+     <center>
+         <hr>
+          <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+          <button type="submit" class="btn btn-primary" value="ADD RECORD">Submit</button>
+     <center>
+    </div>
+</form>
+</div>
+</div>
+   </center> 
+</body>
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
