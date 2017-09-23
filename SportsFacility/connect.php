7c8a269d52a87e4f7cdc6db2f59e25cc59ff62e2
wardo48446 <mpdegraeve@sbcglobal.net>
2017-09-23 18:59:57 +0000

    created connect page to simplify db connection

--- SportsFacility/connect.php
+++ SportsFacility/connect.php
@@ -1,8 +1,8 @@
 <?php // connect.php
 	$host = '127.0.0.1';
-	$db   = 'c9';
-	$user = 'gpeck2217';
-	$pass = '';
+	$db   = 'vv';
+	$user = 'adminacc';
+	$pass = 'admin';
 
 	$conn = mysqli_connect($host, $user, $pass, $db);
 	if(!$conn)
