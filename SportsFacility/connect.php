5bbf0135aba0ec9f951e95320c0a6b193e47ad4d
wardo48446 <mpdegraeve@sbcglobal.net>
2017-09-23 19:08:38 +0000

    modified connect page

--- SportsFacility/connect.php
+++ SportsFacility/connect.php
@@ -1,8 +1,8 @@
 <?php // connect.php
 	$host = '127.0.0.1';
-	$db   = 'vv';
-	$user = 'adminacc';
-	$pass = 'admin';
+	$db   = 'c9';
+	$user = 'gpeck2217';
+	$pass = '';
 
 	$conn = mysqli_connect($host, $user, $pass, $db);
 	if(!$conn)
