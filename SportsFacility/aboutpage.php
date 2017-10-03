af9233bdd235284176d5b99e08da6d507e626d08
wardo48446 <ajsaleh23@oakland.edu>
2017-10-03 15:28:15 +0000

    Added the navbar to about page

--- SportsFacility/aboutpage.php
+++ SportsFacility/aboutpage.php
@@ -1,8 +1,113 @@
 </<!DOCTYPE html>
+<?php
+ include("dbutils.php");
+ session_start();
+ $username = $_POST['username'];
+?>
 <html>
+    <head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="main.css">
+    </head>
     <body>
-        <h1>
-            <p>test</p>
-        </h1>
+         <nav class="navbar navbar-default " >
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="main.php">SportPlex</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+      <li><a href="index.php"><span class="glyphicon glyphicon-home"></span></a></li>
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Schedule<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Events</a></li>
+            <li><a href="#">Times/Dates</a></li>
+            <li><a href="#">Your Schedule</a></li>
+          </ul>
+        </li>
+         <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Pro Shop<span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="proshop.php">Pro Shop</a></li>
+          <li><a href="#">Items in Cart</a></li>
+          <li><a href="#">Searched Items</a></li>
+        </ul>
+        <li><a href="#">Video's</a></li>
+        <li><a href="#">About</a></li>
+         
+        
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+         <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown"></span><span class="caret"></span> <span class="glyphicon glyphicon-user pull-right"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#"> Settings <span class="glyphicon glyphicon-cog pull-right"></span></a></li>
+            <li class="divider"></li>
+            <li><a href="#">User stats <span class="glyphicon glyphicon-stats pull-right"></span></a></li>
+            <li class="divider"></li>
+            <li><a href="#">Messages <span class="badge pull-right"> 42 </span></a></li>
+            <li class="divider"></li>
+            
+            
+            <!-- Trigger Login Modal -->
+              <?php if(isset($_SESSION['loggedin'])){ ?>
+              <li class="active" data-toggle="modal"> <a href="logout.php">Logout</a></li>
+              <?php }else{ ?>
+              <li class="active" data-toggle="modal" data-target="#Login"> <a href="#">Login<span class="glyphicon glyphicon-lock pull-right"></span></a></li>
+              <?php } ?>
+              <!-- End Trigger-->
+            
+           
+          </ul>
+        </li>
+      <!-- This addes a dropdown menu for the important icons -->
+       <ul class="nav navbar-nav navbar-right">
+         <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown"></span><span class="caret"></span> <span class="glyphicon glyphicon-bell pull-right"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#"> Notification1 </a></li>
+            <li class="divider"></li>
+            <li><a href="#">Notification2 </a></li>
+            <li class="divider"></li>
+            <li><a href="#">Notification3 </a></li>
+            <li class="divider"></li>
+            
+          </ul>
+        </li>
+       <!-- End of adding dropdown menu --> 
+  
+      <!-- Username display -->
+       <?php 
+       $username= $_SESSION['login'];
+      $customer = check_login_type($username);
+       if(isset($_SESSION['loggedin'])){
+          if ($customer)
+          {
+          echo "<li><a href='customerpage.php'>" .$_SESSION['login']. "</a> </li>";
+          } else {
+          echo "<li><a href='employeepage.php'>" .$_SESSION['login']. "</a> </li>";
+           } }else{ ?>
+          <li  data-toggle="modal" data-target=""> <a href="main.php">Create Account</a></li>
+           <?php } ?>
+      
+      <!--End of username display -->
+      </ul>
+    </div>
+  </div>
+
+</nav>
+        <h1>About Us</h1>
+            <p>Information about what we offer to our customers and what they can do on our site.</p>
+        <h1>Contact Us</h1>
+            <p>Our contact information will be displayed here. We will list our address, phone number, and hours of opperation.</p>
+        <h1>Other information</h1>
+            <p>other information here</p>
+        
     </body>
 </html>
