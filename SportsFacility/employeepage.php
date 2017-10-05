441e31f56c4bf15c8db0bb0ccc2a7a8a76710da9
wardo48446 <mpdegraeve@sbcglobal.net>
2017-10-05 23:24:51 +0000

    Added the navigation bar to employee page

--- SportsFacility/employeepage.php
+++ SportsFacility/employeepage.php
@@ -2,7 +2,7 @@
 <?php
  include("dbutils.php");
  session_start();
-      if (count($_POST)) 
+      if (count($_POST))
     {
 
 	    echo "Found " . count($_POST) . " elements" . "<td>";
@@ -22,81 +22,72 @@
   <meta name="author" content="">
 
   <title>HockeyPlex Employee</title>
-  
+
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-  
+
   <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
   <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
   <script type = "text/javascript" src = "chk.js"></script>
+  <!-- referencing an external style sheet. -->
   <link rel="stylesheet" type="text/css" href="main.css">
 
 
 </head>
 
 <body>
- <nav class="navbar navbar-default " >
-  <div class="container-fluid">
+
+  <!-- This allows the navbar to stay positioned at the top of the screen on scroll -->
+ <nav class="navbar navbar-inverse navbar-fixed-top" data-spy="affix" data-offset-top="197">
+   <div class="container-fluid">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
+        <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="main.php">SportPlex</a>
+      <a class="navbar-brand" href="main.php">HockeyPlex</a>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
-      <li><a href="index.php"><span class="glyphicon glyphicon-home"></span></a></li>
-        <li class="dropdown">
-          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Schedule<span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Events</a></li>
-            <li><a href="#">Times/Dates</a></li>
-            <li><a href="#">Your Schedule</a></li>
-          </ul>
-        </li>
-         <li class="dropdown">
-        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Pro Shop<span class="caret"></span></a>
-        <ul class="dropdown-menu">
-          <li><a href="proshop.php">Pro Shop</a></li>
-          <li><a href="#">Items in Cart</a></li>
-          <li><a href="#">Searched Items</a></li>
-        </ul>
-        <li><a href="#">Video's</a></li>
-       
-        <li><a href="#">About</a></li>
-         <li><a href="#"><span class="glyphicon glyphicon-mail"></span></a></li>
-        
+      <li><a href="index.php">Home<span class="glyphicon glyphicon-home pull-left"></span></button></a></li>
+      <li><a href="schedulepage.php">Schedule<span class="glyphicon glyphicon-list-alt pull-left"></span></a></li>
+      <li><a href="proshop.php">Pro Shop<span class="glyphicon glyphicon-piggy-bank pull-left"></span></a><li>
+      <li><a href="videopage.php">Video's<span class="glyphicon glyphicon-facetime-video pull-left"></span></a></li>
+      <li><a href="aboutpage.php">About<span class="glyphicon glyphicon-apple pull-left"></span></a><li>
+
       </ul>
-      <ul class="nav navbar-nav navbar-right">
-         <!-- This addes a dropdown menu for the important icons -->
-       <ul class="nav navbar-nav navbar-right">
-         <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown"></span><span class="caret"></span> <span class="glyphicon glyphicon-bell pull-right"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#"> Notification1 </a></li>
+	    <ul class="nav navbar-nav navbar-right">
+	      <ul class="nav navbar-nav">
+    <!--Styled the navbar drop down so it has different sections. -->
+      <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Notifications <span class="caret"></span><span class="glyphicon glyphicon-bell pull-left"></span></a>
+        <ul class="dropdown-menu">
+			<li class="dropdown-header">Important</li>
+            <li><a href="#"> Security Breach </a></li>
             <li class="divider"></li>
-            <li><a href="#">Notification2 </a></li>
+            <li><a href="#">DDOS Attack </a></li>
             <li class="divider"></li>
-            <li><a href="#">Notification3 </a></li>
+			<li class="dropdown-header">Doesn't need attention</li>
+            <li><a href="#"> Skater </a></li>
             <li class="divider"></li>
-            
+
           </ul>
-        </li>
-       <!-- End of adding dropdown menu --> 
-       
-        <!--Dropdown for the user icon for differnt options -->
-         <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown"></span><span class="caret"></span> <span class="glyphicon glyphicon-user pull-right"></span></a>
-          <ul class="dropdown-menu">
+      </li>
+
+
+         <ul class="nav navbar-nav navbar-right">
+          <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Menu</span><span class="caret"></span> <span class="glyphicon glyphicon-globe pull-left"></span></a>
+           <ul class="dropdown-menu">
+			<li class="dropdown-header">Options</li>
             <li><a href="#"> Settings <span class="glyphicon glyphicon-cog pull-right"></span></a></li>
             <li class="divider"></li>
             <li><a href="#">User stats <span class="glyphicon glyphicon-stats pull-right"></span></a></li>
             <li class="divider"></li>
             <li><a href="#">Messages <span class="badge pull-right"> 42 </span></a></li>
             <li class="divider"></li>
-          <!-- End of drop down for users -->  
-            
+
+
             <!-- Trigger Login Modal -->
               <?php if(isset($_SESSION['loggedin'])){ ?>
               <li class="active" data-toggle="modal"> <a href="logout.php">Logout</a></li>
@@ -104,23 +95,22 @@
               <li class="active" data-toggle="modal" data-target="#Login"> <a href="#">Login<span class="glyphicon glyphicon-lock pull-right"></span></a></li>
               <?php } ?>
               <!-- End Trigger-->
-            
-           
+
           </ul>
         </li>
-     
-      <!-- Username display. Displays the username in the upper right corner of the screen. -->
+
+
+      <!-- Username display -->
        <?php if(isset($_SESSION['loggedin'])){
           echo "<li><a>" .$_SESSION['login']."</a></li>";?>
           <?php }else{ ?>
-          <li  data-toggle="modal" data-target="#Login"> <a href="main.php">Create Account</a></li>
+          <li data-toggle="modal" data-target="#Login"><a href="main.php"></a></li>
            <?php } ?>
-      
+
       <!--End of username display -->
       </ul>
     </div>
   </div>
-
 </nav>
 <div class="jumbotron">
   <h1><big>Hockey<strong>Plex</strong></big></h1>
@@ -129,7 +119,7 @@
 </div>
   </div>
 
-  
+
 
 
     <!-- Bootstrap core JavaScript
