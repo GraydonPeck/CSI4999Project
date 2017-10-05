8536ddeba51506807ffbd2dbcebf1b5b7a425915
wardo48446 <mpdegraeve@sbcglobal.net>
2017-10-05 23:31:36 +0000

    Added all the elements including the navbar to the schedule page

---
+++ SportsFacility/schedulepage.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<?php
+ include("dbutils.php");
+ session_start();
+      if (count($_POST))
+    {
+
+	    echo "Found " . count($_POST) . " elements" . "<td>";
+        var_dump($_POST);
+        add_user ($_POST['User_name'], $_POST['User_password'], $_POST['User_email'], $_POST['user_type']);
+        edit_employee ($_POST['User_name'], $_POST['employee_fname'], $_POST['employee_lname'], $_POST['employee_type'], $_POST['employee_phone']);
+        header ("Location: employeepage.php");
+    }
+?>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>HockeyPlex Employee</title>
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+  <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+  <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+  <script type = "text/javascript" src = "chk.js"></script>
+  <!-- referencing an external style sheet. -->
+  <link rel="stylesheet" type="text/css" href="main.css">
+
+
+</head>
+
+<body>
+
+  <!-- This allows the navbar to stay positioned at the top of the screen on scroll -->
+ <nav class="navbar navbar-inverse navbar-fixed-top" data-spy="affix" data-offset-top="197">
+   <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="main.php">HockeyPlex</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+      <li><a href="index.php">Home<span class="glyphicon glyphicon-home pull-left"></span></button></a></li>
+      <li><a href="schedulepage.php">Schedule<span class="glyphicon glyphicon-list-alt pull-left"></span></a></li>
+      <li><a href="proshop.php">Pro Shop<span class="glyphicon glyphicon-piggy-bank pull-left"></span></a><li>
+      <li><a href="videopage.php">Video's<span class="glyphicon glyphicon-facetime-video pull-left"></span></a></li>
+      <li><a href="aboutpage.php">About<span class="glyphicon glyphicon-apple pull-left"></span></a><li>
+
+      </ul>
+	    <ul class="nav navbar-nav navbar-right">
+	      <ul class="nav navbar-nav">
+    <!--Styled the navbar drop down so it has different sections. -->
+      <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Notifications <span class="caret"></span><span class="glyphicon glyphicon-bell pull-left"></span></a>
+        <ul class="dropdown-menu">
+			<li class="dropdown-header">Important</li>
+            <li><a href="#"> Security Breach </a></li>
+            <li class="divider"></li>
+            <li><a href="#">DDOS Attack </a></li>
+            <li class="divider"></li>
+			<li class="dropdown-header">Doesn't need attention</li>
+            <li><a href="#"> Skater </a></li>
+            <li class="divider"></li>
+
+          </ul>
+      </li>
+
+
+         <ul class="nav navbar-nav navbar-right">
+          <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Menu</span><span class="caret"></span> <span class="glyphicon glyphicon-globe pull-left"></span></a>
+           <ul class="dropdown-menu">
+			<li class="dropdown-header">Options</li>
+            <li><a href="#"> Settings <span class="glyphicon glyphicon-cog pull-right"></span></a></li>
+            <li class="divider"></li>
+            <li><a href="#">User stats <span class="glyphicon glyphicon-stats pull-right"></span></a></li>
+            <li class="divider"></li>
+            <li><a href="#">Messages <span class="badge pull-right"> 42 </span></a></li>
+            <li class="divider"></li>
+
+
+            <!-- Trigger Login Modal -->
+              <?php if(isset($_SESSION['loggedin'])){ ?>
+              <li class="active" data-toggle="modal"> <a href="logout.php">Logout</a></li>
+              <?php }else{ ?>
+              <li class="active" data-toggle="modal" data-target="#Login"> <a href="#">Login<span class="glyphicon glyphicon-lock pull-right"></span></a></li>
+              <?php } ?>
+              <!-- End Trigger-->
+
+          </ul>
+        </li>
+
+
+      <!-- Username display -->
+       <?php if(isset($_SESSION['loggedin'])){
+          echo "<li><a>" .$_SESSION['login']."</a></li>";?>
+          <?php }else{ ?>
+          <li data-toggle="modal" data-target="#Login"><a href="main.php"></a></li>
+           <?php } ?>
+
+      <!--End of username display -->
+      </ul>
+    </div>
+  </div>
+</nav>
+<div class="jumbotron">
+  <h1><big>Hockey<strong>Plex</strong></big></h1>
+  <h2>This is the Employee page</h2>
+  <p>This is where the employee enters other information.</p>
+</div>
+  </div>
+
+
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+    <script src="script.js"></script>  </body>
+
+</html>
