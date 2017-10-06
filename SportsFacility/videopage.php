4003e2bbd7e2278fcadd32e7b9ab06181307b127
wardo48446 <mpdegraeve@sbcglobal.net>
2017-10-06 00:51:01 +0000

    Added a video page with navigation bar and a description for the page

---
+++ SportsFacility/videopage.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<?php
+ include("dbutils.php");
+ session_start();
+      if (count($_POST))
+    {
+
+	    echo "Found " . count($_POST) . " elements" . "<td>";
+        var_dump($_POST);
+        add_user ($_POST['User_name'], $_POST['User_password'], $_POST['User_email'], $_POST['user_type']);
+        edit_employee ($_POST['User_name'], $_POST['employee_fname'], $_POST['employee_lname'], $_POST['employee_type'], $_POST['employee_phone']);
+        header ("Location: employeepage.php");
+    }
+?>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>HockeyPlex Employee</title>
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+  <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+  <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+  <script type = "text/javascript" src = "chk.js"></script>
+  <!-- referencing an external style sheet. -->
+  <link rel="stylesheet" type="text/css" href="main.css">
+
+
+</head>
+
+<body>
+
+  <!-- This allows the navbar to stay positioned at the top of the screen on scroll -->
+ <nav class="navbar navbar-inverse navbar-fixed-top" data-spy="affix" data-offset-top="197">
+   <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="main.php">HockeyPlex</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+      <li><a href="index.php">Home<span class="glyphicon glyphicon-home pull-left"></span></button></a></li>
+      <li><a href="schedulepage.php">Schedule<span class="glyphicon glyphicon-list-alt pull-left"></span></a></li>
+      <li><a href="proshop.php">Pro Shop<span class="glyphicon glyphicon-piggy-bank pull-left"></span></a><li>
+      <li><a href="videopage.php">Video's<span class="glyphicon glyphicon-facetime-video pull-left"></span></a></li>
+      <li><a href="aboutpage.php">About<span class="glyphicon glyphicon-apple pull-left"></span></a><li>
+</ul>
+
+<!--Start of form where users can search -->
+          <form class="navbar-form navbar-left">
+      <div class="input-group">
+        <input type="text" class="form-control" placeholder="Search">
+        <div class="input-group-btn">
+          <button class="btn btn-default" type="submit">
+            <i class="glyphicon glyphicon-search"></i>
+          </button>
+        </div>
+      </div>
+<!-- End of users search form -->
+
+</form>
+
+
+	    <ul class="nav navbar-nav navbar-right">
+	      <ul class="nav navbar-nav">
+    <!--Styled the navbar drop down so it has different sections. Settings drop down -->
+      <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#"> <span class="caret"></span><span class="glyphicon glyphicon-cog pull-left"></span></a>
+        <ul class="dropdown-menu">
+			<li class="dropdown-header">Settings</li>
+            <li><a href="#"> Help <span class="glyphicon glyphicon-search"></span></a></li>
+            <li class="divider"></li>
+             <li><a href="#">User stats <span class="glyphicon glyphicon-stats pull-right"></span></a></li>
+            <li class="divider"></li>
+            <li><a href="#">Messages <span class="badge pull-right"> 42 </span></a></li>
+            <li class="divider"></li>
+
+          </ul>
+      </li>
+    <!--End of setting dropdown menu -->
+
+         <ul class="nav navbar-nav navbar-right">
+
+
+
+             <!-- Trigger Login Modal -->
+              <?php if(isset($_SESSION['loggedin'])){ ?>
+              <li  data-toggle="modal"> <a href="logout.php">Logout</a></li>
+              <?php }else{ ?>
+              <li  data-toggle="modal" data-target="#Login"> <a href="#">Login<span class="glyphicon glyphicon-lock pull-left"></span></a></li>
+              <?php } ?>
+              <!-- End Trigger-->
+
+
+
+
+      <!-- Username display -->
+       <?php if(isset($_SESSION['loggedin'])){
+          echo "<li><a>" .$_SESSION['login']."</a></li>";?>
+          <?php }else{ ?>
+          <li data-toggle="modal" data-target="#Login"><a href="main.php"></a></li>
+           <?php } ?>
+
+      <!--End of username display -->
+      </ul>
+    </div>
+  </div>
+</nav>
+<div class="jumbotron">
+  <h1><big>Hockey<strong>Plex</strong></big></h1>
+  <h2>This is the video page</h2>
+  <p>This is where the Customer/Employee learn's how to complete each task assigned.</p>
+
+
+</div>
+  </div>
+
+
+<!--Login Modal -->
+
+  <div class="modal fade" id="Login" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+          <h4 class="modal-title" id="myModalLabel">Login</h4>
+        </div>
+        <div class="modal-body">
+
+          <center>
+          <form action = "checklogin.php" method="post">
+                                <input type="hidden" name="User_number">
+          <table>
+          <tr>
+          <td>Username:</td>     <td><input type="text" name="username"></td>
+          </tr>
+          <tr>
+          <td>Password:</td>     <td><input type="password" name="passwd"></td>
+          </tr>
+          </table>
+
+          </center>
+        </div>
+        <div class="modal-footer">
+          <center>
+          <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+          <button type="submit" class="btn btn-primary" value="ADD RECORD">Submit</button>
+          </center>
+        </div>
+      </div>
+    </div>
+  </div>
+          </form>
+
+  <!-- End Login Modal -->
+
+
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+    <script src="script.js"></script>  </body>
+
+</html>
