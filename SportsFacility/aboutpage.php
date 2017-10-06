2b73259d8041504fe892a1233d4becd1a4517069
wardo48446 <mpdegraeve@sbcglobal.net>
2017-10-06 01:15:59 +0000

    Added the navbar

--- SportsFacility/aboutpage.php
+++ SportsFacility/aboutpage.php
@@ -6,106 +6,101 @@
 ?>
 <html>
     <head>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="main.css">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>HockeyPlex Employee</title>
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+  <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+  <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+  <script type = "text/javascript" src = "chk.js"></script>
+  <!-- referencing an external style sheet. -->
+  <link rel="stylesheet" type="text/css" href="main.css">
     </head>
     <body>
-         <nav class="navbar navbar-default " >
-  <div class="container-fluid">
+   <!-- This allows the navbar to stay positioned at the top of the screen on scroll -->
+ <nav class="navbar navbar-inverse navbar-fixed-top" data-spy="affix" data-offset-top="197">
+   <div class="container-fluid">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
+        <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="main.php">SportPlex</a>
+      <a class="navbar-brand" href="main.php">HockeyPlex</a>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
-      <li><a href="index.php"><span class="glyphicon glyphicon-home"></span></a></li>
-        <li class="dropdown">
-          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Schedule<span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Events</a></li>
-            <li><a href="#">Times/Dates</a></li>
-            <li><a href="#">Your Schedule</a></li>
-          </ul>
-        </li>
-         <li class="dropdown">
-        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Pro Shop<span class="caret"></span></a>
+      <li><a href="index.php">Home<span class="glyphicon glyphicon-home pull-left"></span></button></a></li>
+      <li><a href="schedulepage.php">Schedule<span class="glyphicon glyphicon-list-alt pull-left"></span></a></li>
+      <li><a href="proshop.php">Pro Shop<span class="glyphicon glyphicon-piggy-bank pull-left"></span></a><li>
+      <li><a href="videopage.php">Video's<span class="glyphicon glyphicon-facetime-video pull-left"></span></a></li>
+      <li><a href="aboutpage.php">About<span class="glyphicon glyphicon-apple pull-left"></span></a><li>
+</ul>
+
+
+
+</form>
+
+
+	    <ul class="nav navbar-nav navbar-right">
+	      <ul class="nav navbar-nav">
+    <!--Styled the navbar drop down so it has different sections. Settings drop down -->
+      <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Settings <span class="caret"></span><span class="glyphicon glyphicon-cog pull-left"></span></a>
         <ul class="dropdown-menu">
-          <li><a href="proshop.php">Pro Shop</a></li>
-          <li><a href="#">Items in Cart</a></li>
-          <li><a href="#">Searched Items</a></li>
-        </ul>
-        <li><a href="#">Video's</a></li>
-        <li><a href="#">About</a></li>
-         
-        
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-         <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown"></span><span class="caret"></span> <span class="glyphicon glyphicon-user pull-right"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#"> Settings <span class="glyphicon glyphicon-cog pull-right"></span></a></li>
+			<li class="dropdown-header">Settings</li>
+            <li><a href="#"> Help <span class="glyphicon glyphicon-search"></span></a></li>
             <li class="divider"></li>
-            <li><a href="#">User stats <span class="glyphicon glyphicon-stats pull-right"></span></a></li>
+             <li><a href="#">User stats <span class="glyphicon glyphicon-stats pull-right"></span></a></li>
             <li class="divider"></li>
             <li><a href="#">Messages <span class="badge pull-right"> 42 </span></a></li>
             <li class="divider"></li>
-            
-            
-            <!-- Trigger Login Modal -->
+
+          </ul>
+      </li>
+    <!--End of setting dropdown menu -->
+
+         <ul class="nav navbar-nav navbar-right">
+
+
+
+             <!-- Trigger Login Modal -->
               <?php if(isset($_SESSION['loggedin'])){ ?>
-              <li class="active" data-toggle="modal"> <a href="logout.php">Logout</a></li>
+              <li  data-toggle="modal"> <a href="logout.php">Logout</a></li>
               <?php }else{ ?>
-              <li class="active" data-toggle="modal" data-target="#Login"> <a href="#">Login<span class="glyphicon glyphicon-lock pull-right"></span></a></li>
+              <li  data-toggle="modal" data-target="#Login"> <a href="#">Login<span class="glyphicon glyphicon-lock pull-left"></span></a></li>
               <?php } ?>
               <!-- End Trigger-->
-            
-           
-          </ul>
-        </li>
-      <!-- This addes a dropdown menu for the important icons -->
-       <ul class="nav navbar-nav navbar-right">
-         <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown"></span><span class="caret"></span> <span class="glyphicon glyphicon-bell pull-right"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#"> Notification1 </a></li>
-            <li class="divider"></li>
-            <li><a href="#">Notification2 </a></li>
-            <li class="divider"></li>
-            <li><a href="#">Notification3 </a></li>
-            <li class="divider"></li>
-            
-          </ul>
-        </li>
-       <!-- End of adding dropdown menu --> 
-  
+
+
+
+
       <!-- Username display -->
-       <?php 
-       $username= $_SESSION['login'];
-      $customer = check_login_type($username);
-       if(isset($_SESSION['loggedin'])){
-          if ($customer)
-          {
-          echo "<li><a href='customerpage.php'>" .$_SESSION['login']. "</a> </li>";
-          } else {
-          echo "<li><a href='employeepage.php'>" .$_SESSION['login']. "</a> </li>";
-           } }else{ ?>
-          <li  data-toggle="modal" data-target=""> <a href="main.php">Create Account</a></li>
+       <?php if(isset($_SESSION['loggedin'])){
+          echo "<li><a>" .$_SESSION['login']."</a></li>";?>
+          <?php }else{ ?>
+          <li data-toggle="modal" data-target="#Login"><a href="main.php"></a></li>
            <?php } ?>
-      
+
       <!--End of username display -->
       </ul>
     </div>
   </div>
-
 </nav>
-        <h1>About Us</h1>
+
+<h1>About Us</h1>
+
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
+
                     <h2>How we came to be ...</h2>
                     <p>Twenty years ago, here in the middle of town, we looked forward to winter. That is when the low lying area in the back of our sub froze over and became our makeshift ice rink. It became our mission as winter approached that this area was to be as full of water as possible. We would spend hours skating, shooting, and even shared with the neighborhood kids so they could enjoy too! One of our dads got a job as a zamboni driver a few towns over. He let us take a ride when was working once and our vision was sealed. We were going to create our own ice arena in our town for our community to enjoy and be proud of. Fast foward to today ... our dream is your reality: HockeyPlex</p>
                     <h2>Here is what you can do on our site ...</h2>
@@ -132,4 +127,16 @@
                 </div>
         </div>
     </body>
+
+
+     <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+    <script src="script.js"></script>
 </html>
