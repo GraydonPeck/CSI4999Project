5ed144951db03558e7cfe4134b8fe24b3674856e
wardo48446 <mpdegraeve@sbcglobal.net>
2017-10-06 01:51:06 +0000

    Added the navigation bar to proshop.php, and login modal

--- SportsFacility/proshop.php
+++ SportsFacility/proshop.php
@@ -14,7 +14,8 @@ include("dbutils.php");
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <link rel="stylesheet" type="text/css" href="proshop.css">
 <script type = "text/javascript" src = "chk.js"></script>
-
+<!-- referencing an external style sheet. -->
+  <link rel="stylesheet" type="text/css" href="main.css">
 </head>
 <?php
 $db_handle = new DBController();
@@ -24,7 +25,7 @@ switch($_GET["action"]) {
 		if(!empty($_POST["product_stock"])) {
 			$productByCode = $db_handle->runQuery("SELECT * FROM product_db WHERE product_code='" . $_GET["product_code"] . "'");
 			$itemArray = array($productByCode[0]["product_code"]=>array('product_name'=>$productByCode[0]["product_name"], 'product_code'=>$productByCode[0]["product_code"], 'product_quantity'=>$_POST["product_quantity"], 'product_price'=>$productByCode[0]["product_price"]));
-			
+
 			if(!empty($_SESSION["cart_item"])) {
 				if(in_array($productByCode[0]["product_code"],array_keys($_SESSION["cart_item"]))) {
 					foreach($_SESSION["cart_item"] as $k => $v) {
@@ -47,7 +48,7 @@ switch($_GET["action"]) {
 		if(!empty($_SESSION["cart_item"])) {
 			foreach($_SESSION["cart_item"] as $k => $v) {
 					if($_GET["product_code"] == $k)
-						unset($_SESSION["cart_item"][$k]);				
+						unset($_SESSION["cart_item"][$k]);
 					if(empty($_SESSION["cart_item"]))
 						unset($_SESSION["cart_item"]);
 			}
@@ -55,108 +56,140 @@ switch($_GET["action"]) {
 	break;
 	case "empty":
 		unset($_SESSION["cart_item"]);
-	break;	
+	break;
 }
 }
 
 ?>
 <body>
-  <nav class="navbar navbar-default " >
-  <div class="container-fluid">
+  <!-- This allows the navbar to stay positioned at the top of the screen on scroll -->
+ <nav class="navbar navbar-inverse navbar-fixed-top" data-spy="affix" data-offset-top="197">
+   <div class="container-fluid">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
+        <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="main.php">SportPlex</a>
+      <a class="navbar-brand" href="main.php">HockeyPlex</a>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
-      <li><a href="index.php"><span class="glyphicon glyphicon-home"></span></a></li>
-        <li class="dropdown">
-          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Schedule<span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Events</a></li>
-            <li><a href="#">Times/Dates</a></li>
-            <li><a href="#">Your Schedule</a></li>
-          </ul>
-        </li>
-         <li class="dropdown">
-        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Pro Shop<span class="caret"></span></a>
+      <li><a href="index.php">Home<span class="glyphicon glyphicon-home pull-left"></span></button></a></li>
+      <li><a href="schedulepage.php">Schedule<span class="glyphicon glyphicon-list-alt pull-left"></span></a></li>
+      <li><a href="proshop.php">Pro Shop<span class="glyphicon glyphicon-piggy-bank pull-left"></span></a><li>
+      <li><a href="videopage.php">Video's<span class="glyphicon glyphicon-facetime-video pull-left"></span></a></li>
+      <li><a href="aboutpage.php">About<span class="glyphicon glyphicon-apple pull-left"></span></a><li>
+</ul>
+<!--Start of form where users can search -->
+          <form class="navbar-form navbar-left">
+      <div class="input-group">
+        <input type="text" class="form-control" placeholder="Search">
+        <div class="input-group-btn">
+          <button class="btn btn-default" type="submit">
+            <i class="glyphicon glyphicon-search"></i>
+          </button>
+        </div>
+      </div>
+<!-- End of users search form -->
+
+</form>
+
+
+
+
+
+	    <ul class="nav navbar-nav navbar-right">
+	      <ul class="nav navbar-nav">
+    <!--Styled the navbar drop down so it has different sections. Settings drop down -->
+      <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Settings <span class="caret"></span><span class="glyphicon glyphicon-cog pull-left"></span></a>
         <ul class="dropdown-menu">
-          <li><a href="proshop.php">Pro Shop</a></li>
-          <li><a href="#">Cart</a></li>
-          <li><a href="#">Searched Items</a></li>
-        </ul>
-        <li><a href="#">Video's</a></li>
-        <li><a href="#">About</a></li>
-         
-        
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-         <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown"></span><span class="caret"></span> <span class="glyphicon glyphicon-user pull-right"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#"> Settings <span class="glyphicon glyphicon-cog pull-right"></span></a></li>
+			<li class="dropdown-header">Settings</li>
+            <li><a href="#"> Help <span class="glyphicon glyphicon-search"></span></a></li>
             <li class="divider"></li>
-            <li><a href="#">User stats <span class="glyphicon glyphicon-stats pull-right"></span></a></li>
+             <li><a href="#">User stats <span class="glyphicon glyphicon-stats pull-right"></span></a></li>
             <li class="divider"></li>
             <li><a href="#">Messages <span class="badge pull-right"> 42 </span></a></li>
             <li class="divider"></li>
-            
-            
-            <!-- Trigger Login Modal -->
+
+          </ul>
+      </li>
+    <!--End of setting dropdown menu -->
+
+         <ul class="nav navbar-nav navbar-right">
+
+
+
+             <!-- Trigger Login Modal -->
               <?php if(isset($_SESSION['loggedin'])){ ?>
-              <li class="active" data-toggle="modal"> <a href="logout.php">Logout</a></li>
+              <li  data-toggle="modal"> <a href="logout.php">Logout</a></li>
               <?php }else{ ?>
-              <li class="active" data-toggle="modal" data-target="#Login"> <a href="#">Login<span class="glyphicon glyphicon-lock pull-right"></span></a></li>
+              <li  data-toggle="modal" data-target="#Login"> <a href="#">Login<span class="glyphicon glyphicon-lock pull-left"></span></a></li>
               <?php } ?>
               <!-- End Trigger-->
-            
-           
-          </ul>
-        </li>
-      <!-- This addes a dropdown menu for the important icons -->
-       <ul class="nav navbar-nav navbar-right">
-         <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown"></span><span class="caret"></span> <span class="glyphicon glyphicon-bell pull-right"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#"> Notification1 </a></li>
-            <li class="divider"></li>
-            <li><a href="#">Notification2 </a></li>
-            <li class="divider"></li>
-            <li><a href="#">Notification3 </a></li>
-            <li class="divider"></li>
-            
-          </ul>
-        </li>
-       <!-- End of adding dropdown menu --> 
-  
+
+
+
+
       <!-- Username display -->
        <?php if(isset($_SESSION['loggedin'])){
-          if ($customer){
-          echo "<li><a href='customerpage.php'>" .$_SESSION['login']. "</a> </li>";?>
-          <?php } else { ?>
-          <?php
-           echo "<li><a href='employeepage.php'>" .$_SESSION['login']. "</a> </li>";?>
-          <?php } ?>
+          echo "<li><a>" .$_SESSION['login']."</a></li>";?>
           <?php }else{ ?>
-          <li  data-toggle="modal" data-target=""> <a href="main.php">Create Account</a></li>
+          <li data-toggle="modal" data-target="#Login"><a href="main.php"></a></li>
            <?php } ?>
-      
+
       <!--End of username display -->
       </ul>
     </div>
   </div>
-
 </nav>
+
+<!--Login Modal -->
+
+  <div class="modal fade" id="Login" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+          <h4 class="modal-title" id="myModalLabel">Login</h4>
+        </div>
+        <div class="modal-body">
+
+          <center>
+          <form action = "checklogin.php" method="post">
+                                <input type="hidden" name="User_number">
+          <table>
+          <tr>
+          <td>Username:</td>     <td><input type="text" name="username"></td>
+          </tr>
+          <tr>
+          <td>Password:</td>     <td><input type="password" name="passwd"></td>
+          </tr>
+          </table>
+
+          </center>
+        </div>
+        <div class="modal-footer">
+          <center>
+          <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+          <button type="submit" class="btn btn-primary" value="ADD RECORD">Submit</button>
+          </center>
+        </div>
+      </div>
+    </div>
+  </div>
+          </form>
+
+  <!-- End Login Modal -->
 <div id="shopping-cart">
 <div class="txt-heading">Shopping Cart <a id="btnEmpty" href="proshop.php?action=empty">Empty Cart</a></div>
 <?php
 if(isset($_SESSION["cart_item"])){
     $item_total = 0;
-?>	
+?>
 <table cellpadding="5" cellspacing="1">
 <tbody>
 <tr>
@@ -165,8 +198,8 @@ if(isset($_SESSION["cart_item"])){
 <th style="text-align:right;"><strong>Code</strong></th>
 <th style="text-align:right;"><strong>Quantity</strong></th>
 <th style="text-align:center;"><strong>Description</strong></th>
-</tr>	
-<?php		
+</tr>
+<?php
     foreach ($_SESSION["cart_item"] as $item){
 		?>
 				<tr>
@@ -186,7 +219,7 @@ if(isset($_SESSION["cart_item"])){
 <td colspan="5" align=right><strong>Total:</strong> <?php echo "$".$item_total; ?></td>
 </tr>
 </tbody>
-</table>		
+</table>
   <?php
 }
 ?>
@@ -196,7 +229,7 @@ if(isset($_SESSION["cart_item"])){
 	<div class="txt-heading">Products</div>
 	<?php
 	$product_array = $db_handle->runQuery("SELECT * FROM product_db ORDER BY product_code ASC");
-	if (!empty($product_array)) { 
+	if (!empty($product_array)) {
 		foreach($product_array as $key=>$value){
 	?>
 		<div class="product-item">
@@ -212,4 +245,15 @@ if(isset($_SESSION["cart_item"])){
 	?>
 </div>
 </body>
+
+ <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+    <script src="script.js"></script>
 </html>
