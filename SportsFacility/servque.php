88c34aaef3c6bf267c06b633ce09efa3c3362175
cneil89 <ceneil@oakland.edu>
2017-10-19 20:16:32 -0400

    added js to serv que page

--- SportsFacility/servque.php
+++ SportsFacility/servque.php
@@ -263,6 +263,9 @@ _END;
 				</div>	
 
 		</div>
+		
+		<script type='text/javascript' src="js/jquery.js"></script>
+		<script type='text/javascript' src="js/bootstrap.js"></script>		
 
 	</body>
 </html>
